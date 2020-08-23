626f05330931df5bb5efcd9ba0d79654d724466e
bmartin97 <barsimartin@gmail.com>
2020-08-23 23:45:41 +0200

    separate live and dev build webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,54 @@
 const path = require('path');
 
-module.exports = (env) => {
-    return {
-        mode: 'production',
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'choosely.min.js',
-            libraryTarget: 'commonjs2'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js?$/,
-                    exclude: /(node_modules)/,
-                    use: 'babel-loader'
+module.exports = (env, argv) => {
+    switch (argv.mode) {
+        case 'production':
+            return {
+                mode: 'production',
+                entry: './src/index.js',
+                output: {
+                    path: path.resolve(__dirname, 'dist'),
+                    filename: 'choosely.min.js',
+                    libraryTarget: 'commonjs2'
+                },
+                module: {
+                    rules: [
+                        {
+                            test: /\.js?$/,
+                            exclude: /(node_modules)/,
+                            use: 'babel-loader'
+                        }
+                    ]
+                },
+                resolve: {
+                    extensions: ['.js']
                 }
-            ]
-        },
-        resolve: {
-            extensions: ['.js']
-        }
-    };
+            };
+        case 'development':
+            return {
+                mode: 'development',
+                entry: './src/index.js',
+                output: {
+                    path: path.resolve(__dirname, 'dist'),
+                    filename: 'choosely.dev.js',
+                    libraryTarget: 'commonjs2'
+                },
+                devtool: 'eval-source-map',
+                module: {
+                    rules: [
+                        {
+                            test: /\.js?$/,
+                            exclude: /(node_modules)/,
+                            use: 'babel-loader'
+                        }
+                    ]
+                },
+                resolve: {
+                    extensions: ['.js']
+                }
+            };
+        default:
+            console.log('Missing --mode arg!');
+            return;
+    }
 };
